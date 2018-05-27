2034e7257b14dc82cd58d74437e8501ee2e1e7b3
liguoping <liguoping01@58ganji.com>
2018-05-27 17:45:47 +0800

    add

--- src/main/java/com/gshy/web/service/enums/ImageTypeEnum.java
+++ src/main/java/com/gshy/web/service/enums/ImageTypeEnum.java
@@ -7,8 +7,8 @@ import java.util.List;
 import java.util.Map;
 
 public enum ImageTypeEnum {
-	AdvanceMoney(1, "房抵"), 
-	Mortgage(2, "垫资");
+	Mortgage(1, "房抵"),
+	AdvanceMoney(2, "垫资"); 
 	
 	private int value;
 	private String name;
